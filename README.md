7c50d144526413374c8ba750ba2bbf5e4d8c364e
hehe <2154217237@qq.com>
2017-07-21 17:57:45 +0800

    添加了清除已完成功能

--- README.md
+++ README.md
@@ -3,7 +3,16 @@ some facilities about todomvc
 
 ### 测试学习
 
-# 测试如何将项目托管到git上
+#### 测试如何将项目托管到git上
 
-
+vue.js是框架。 它更适合手机端的WEB开发，与angular.js相同的是 声明式开发，但性能高于angular，体积小很多。虽说是华人，但别人的文档都是英文的，范例还比较少。
+这么说吧，框架就像一个毛坯房，主框架已经建成了，你只要按照你的意思装修就行，因为房子都长那样，当然你也可以自己写，自己建房子的速度你觉得和已经建好的那个更好呢？
+依个人口味选择
+前后台交互其实都是通过servlet来实现的。 即servlet写数据给前台，前台展示。前台提交数据，servlet处理。 框架只是封装了servlet，提供了更加简便，更加好维护的集成模式。如果你很感兴趣框架的实现方式的话，你可以看看nutz的源代码。 nutz是...
+首先，我们先了解什么是MVX框架模式？ MVX框架模式：MVC+MVP+MVVM 1.MVC：Model(模型)+View(视图)+controller(控制器)，主要是基于分层的目的，让彼此的职责分开。 View通过Controller来和Model联系，Controller是View和Model的协调者，View和Mo...
+Vue.js与其他框架的区别： 1.与AngularJS的区别 相同点： 都支持指令：内置指令和自定义指令。 都支持过滤器：内置过滤器和自定义过滤器。 都支持双向数据绑定。 都不支持低端浏览器。 不同点： 1.AngularJS的学习成本高，比如增加了Dependency ...
+精 简 易上手
+Vue.js与其他框架的区别？ 1.与AngularJS的区别 相同点： 都支持指令：内置指令和自定义指令。 都支持过滤器：内置过滤器和自定义过滤器。 都支持双向数据绑定。 都不支持低端浏览器。 不同点： 1.AngularJS的学习成本高，比如增加了Dependency
+我在慕课网上看到过开发饿了吗webAPP的。
+vue的ui框架除了VUX用的比较多， 还有这些框架： js框架就jQuery、zepto等等吧，mv*的就angular、backbone等等吧，css就jQuery ui、bootstrap等等吧。
 
--- js/app.js
+++ js/app.js
@@ -6,14 +6,15 @@
 	var app = angular.module("todomvc",[]);
 	//创建控制器
 	app.controller("todomvcCtrl",['$scope',function($scope){
+		//任务展示
 		$scope.todos = [
-			{id:1, title: '吃饭',complete:false},
-			{id:2, title: '睡觉',complete:false},
-			{id:3, title: '打豆豆',complete:true},
+			{id:1, title: '吃饭',complete:true},
+			{id:2, title: '睡觉',complete:true},
+			{id:3, title: '打豆豆',complete:false},
 			{id:4, title: '学习',complete:false},
 			{id:5, title: '思考',complete:true}
-		],
-		//增加项目
+		];
+		//增加项目任务
 		$scope.addTitle = function(){
 			if(!$scope.newTitle){
 				return;
@@ -24,8 +25,8 @@
 				complete:false
 			});
 			$scope.newTitle = '';
-		}
-		//删减项目
+		};
+		//删减项目任务
 		$scope.removeTitle = function(id){
 			for(var i=0;i<$scope.todos.length;i++){
 				var item = $scope.todos[i];
@@ -34,16 +35,45 @@
 					return;
 				}
 			}
-		}
-		//修改项目
+		};
+		//修改项目任务
 		$scope.isEditId = -1;
 		$scope.edit = function(id){
 			$scope.isEditId = id;
-		}
+		};
 		$scope.save = function(id){
 			$scope.isEditId = -1;
+		};
+		//勾选项目任务
+		//全选全不选
+		$scope.isAllSelect = false;
+		$scope.allSelect = function(){
+			for(var i=0;i<$scope.todos.length;i++){
+				var item = $scope.todos[i];
+				item.complete = $scope.isAllSelect;
+			}
 		}
-
+		//显示未完成的任务数
+		$scope.getActive = function(){
+			var count = 0;
+			for(var i=0;i<$scope.todos.length;i++){
+				var item = $scope.todos[i];
+				if(!item.complete){
+					count++;
+				}
+			}
+			return count;
+		}
+		//清除已完成的项目任务
+		$scope.clearCompleted = function(){
+			for(var i=$scope.todos.length-1;i>=0;i--){
+				var item = $scope.todos[i];
+				if(item.complete){
+					$scope.todos.splice(i,1);//splice会改变原数组,原数组的length会发生改变，所以采用i--,而不是i++
+				}
+			}
+		};
+		//切换显示不同状态的任务
 	}])
 
 })(angular);
